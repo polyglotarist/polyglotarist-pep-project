1afc886e3b1d5d2de47c9191d6fa1ef475c66dca
polyglotarist <majid.samir2@gmail.com>
2024-10-31 22:56:06 +0000

    cleaned up some of the comments, all tests pass

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -61,11 +61,9 @@ public class SocialMediaController {
 
     // 2: Our API should be able to process User logins.
     private void getLoginHandler(Context ctx) throws JsonProcessingException {
+
         ObjectMapper objectMapper = new ObjectMapper();
         Account loginRequest = objectMapper.readValue(ctx.body(), Account.class);
-
-        System.out.println("Received login request.");
-
         boolean isValidUser = accountService.validateUser(loginRequest.getUsername(), loginRequest.getPassword());
         if (isValidUser) {
             // Account account = new Account(loginRequest.getAccount_id(), loginRequest.getUsername(), loginRequest.getPassword());
@@ -79,14 +77,11 @@ public class SocialMediaController {
     }
 
     // 3: Our API should be able to process the creation of new messages.
-    //The creation of the message will be successful if and only if the message_text is not blank, is not over 255 characters,
-    // and posted_by refers to a real, existing user
-
     private void addMessageHandler(Context ctx) throws JsonProcessingException {
+
         ObjectMapper objectMapper = new ObjectMapper();
         Message message = objectMapper.readValue(ctx.body(), Message.class);
         System.out.println("Received new message request.");
-
 
         if(message.getMessage_text().length() > 0 && message.getMessage_text().length() <= 255 
         && accountService.accountExists(message.getPosted_by())){
@@ -98,13 +93,9 @@ public class SocialMediaController {
         }else{
             ctx.status(400);
         }
-        
     }
 
-    // 4: Our API should be able to retrieve all messages.
-    /**As a user, I should be able to submit a GET request on the endpoint GET localhost:8080/messages.
-- The response body should contain a JSON representation of a list containing all messages retrieved from the database.
- It is expected for the list to simply be empty if there are no messages. The response status should always be 200, which is the default. */
+// 4: Our API should be able to retrieve all messages.
     private void getAllMessagesHandler(Context ctx) {
         List<Message> messages = messageService.getAllMessages();
         ctx.json(messages);
@@ -113,10 +104,6 @@ public class SocialMediaController {
     }
 
 // 5: Our API should be able to retrieve a message by its ID.
-/*As a user, I should be able to submit a GET request on the endpoint GET localhost:8080/messages/{message_id}.
-- The response body should contain a JSON representation of the message identified by the message_id.
- It is expected for the response body to simply be empty if there is no such message. 
- The response status should always be 200, which is the default. */
     private void getMessageByIdHandler(Context ctx) {
         int messageId = Integer.parseInt(ctx.pathParam("id"));
         Message retrievedMessage = messageService.getMessageById(messageId); 
@@ -126,15 +113,7 @@ public class SocialMediaController {
         }
     }
 
-    // 6: Our API should be able to delete a message identified by a message ID.
-    /*As a User, I should be able to submit a DELETE request on the endpoint DELETE localhost:8080/messages/{message_id}.
-
-- The deletion of an existing message should remove an existing message from the database. 
-If the message existed, the response body should contain the now-deleted message. 
-The response status should be 200, which is the default.
-- If the message did not exist, the response status should be 200, but the response body should be empty.
- This is because the DELETE verb is intended to be idempotent, ie, multiple calls to the DELETE endpoint should respond 
- with the same type of response. */
+// 6: Our API should be able to delete a message identified by a message ID.
     private void deleteMessageHandler(Context ctx) {
         int messageId = Integer.parseInt(ctx.pathParam("id"));
         Message deletedMessage = messageService.getMessageById(messageId);
@@ -146,19 +125,8 @@ The response status should be 200, which is the default.
     }
 
 // 7: Our API should be able to update a message text identified by a message ID.
-/*As a user, I should be able to submit a PATCH request on the endpoint PATCH localhost:8080/messages/{message_id}.
-The request body should contain a new message_text values to replace the message identified by message_id.
-The request body can not be guaranteed to contain any other information.
-- The update of a message should be successful if and only if the message id already exists and the new message_text is not blank 
-and is not over 255 characters.
-If the update is successful, the response body should contain the full updated message (including message_id, posted_by, message_text,
-and time_posted_epoch), and the response status should be 200, which is the default. 
-The message existing on the database should have the updated message_text.
-- If the update of the message is not successful for any reason, the response status should be 400. (Client error) */
     private void updateMessageHandler(Context ctx) throws JsonProcessingException {
 
-
-    //Let's try a simpler approach. Commented code below fails updateMessageMessageNotFound
     //step1: retrieve what's passed in the ctx object, namely the id, and the newText
     int retrievedId = Integer.parseInt(ctx.pathParam("message_id"));
     //step2: validate that the id corresponds to an existing message
@@ -197,55 +165,9 @@ The message existing on the database should have the updated message_text.
     }else {
         ctx.status(400);
     }
-
-        // //retrieve the new text from the ctx object
-        // String newText = ctx.bodyAsClass(Message.class).getMessage_text();
-        //  //check if new message text is valid: must be not be blank and must be less than 256
-        //  boolean isValidText = false;
-        //  if(newText != null){
-        //     if(newText.length() > 0 && newText.length() < 256 ){
-        //         isValidText = true;
-        //     }
-        //  }else if(newText == null){
-        //     //the newText is null so signal ctx.status(400)
-        //     ctx.status(400);
-        //  }  
-
-        // //retrieve the id from ctx object
-        // int messageId = Integer.parseInt(ctx.pathParam("id"));
-        // Message nonUpdatedMessage = messageService.getMessageById(messageId);
-        // String oldText = nonUpdatedMessage.getMessage_text();
-        //   //check if the messageId exists:
-        //   boolean isValidId = false;
-        //   if(nonUpdatedMessage != null){
-        //       isValidId = true;
-        //   }
-        // //reference the non updated object temporarily
-        // Message theNewMessageObject = nonUpdatedMessage;
-
-        // //perform the update if id is valid and text is valid:
-        // if(isValidId && isValidText){
-        //     //update the text on the non updated message object
-        //     nonUpdatedMessage.setMessage_text(newText);
-        //     //pass the Message object with updated messsage_text to the service layer and store in theNewMessageObject:
-        //     theNewMessageObject = messageService.updateMessage(nonUpdatedMessage);
-        // }
-        // //if update is successful, return status 200 and json of updated message object:
-        // if(theNewMessageObject != null && theNewMessageObject.getMessage_text() != oldText){
-        //     ctx.json(theNewMessageObject);
-        //     ctx.status(200);
-
-        // }else{
-        //     ctx.status(400);
-        // }
-    }
+}
 
     // 8: Our API should be able to retrieve all messages written by a particular user.
-    /*As a user, I should be able to submit a GET request on the endpoint GET localhost:8080/accounts/{account_id}/messages.
-- The response body should contain a JSON representation of a list containing all messages posted by a particular user, 
-which is retrieved from the database. It is expected for the list to simply be empty if there are no messages.
- The response status should always be 200, which is the default. */
-
     private void getMessagesByUserHandler(Context ctx) {
         int accountId = Integer.parseInt(ctx.pathParam("account_id"));
         List<Message> messages = messageService.getMessagesByUser(accountId);
